2ca8137f18ab5bb2c50cfbc9bab6811c4417fb42
andypeeters <andy@studiopasokon.com>
2023-06-11 16:34:41 +0200

    feat(scraper): add hello world python file
    Basic python script and VSCode launch files are added as a base for later
    development.

---
+++ src/duolingo-scraper.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2023, Andy Peeters <andy@studiopasokon.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program, see LICENSE.
+# If not, see <https://www.gnu.org/licenses/gpl-3.0.txt>.
+
+# Main program starts here.
+if __name__ == '__main__':
+    print("Hello, world!")
