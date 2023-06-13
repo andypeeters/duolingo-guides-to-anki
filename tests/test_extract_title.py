0bdad0573e172f1e52df5528fa1eff3572181685
andypeeters <andy@studiopasokon.com>
2023-06-13 21:22:07 +0200

    test: set up unit testing
    Added a draft unit test as a preparation to be able to fully test the
    scraper.

---
+++ tests/test_extract_title.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2023, Andy Peeters <andy@studiopasokon.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program, see LICENSE.
+# If not, see <https://www.gnu.org/licenses/gpl-3.0.txt>.
+
+import unittest
+from ..src import duolingo_scraper
+
+class Test_TestHtmlExtration(unittest.TestCase):
+    def test_extract_title(self):
+        self.assertEqual(duolingo_scraper.extract_document_title(), 4)
+
+if __name__ == '__main__':
+    unittest.main()
